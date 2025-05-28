4c175941eb6d16aed85268136e738cbad11daae8
Gil Jeremy Ditablan <71557609+destinyarx@users.noreply.github.com>
2025-05-28 20:30:12 +0800

    fix middleware for auth and non auth user

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,4 +1,6 @@
-import { defineNuxtRouteMiddleware, navigateTo } from '#app'
+import { defineNuxtRouteMiddleware, navigateTo } from '#app';
+import { useUserStore } from '@/stores/authStore';
+
 
 const publicLinks = [
     "/",
@@ -8,11 +10,22 @@ const publicLinks = [
     "/contact-us"
 ];
 
-export default defineNuxtRouteMiddleware((to, from) => {
-    const user = useSupabaseUser();
+export default defineNuxtRouteMiddleware(async (to, from) => {
+    const user = useUserStore()
+    const authUser = useSupabaseUser();
+
+    if (authUser && !user.profile?.id) {
+        await user.setUserInfo();
+    }
+
+    
 
     // If there is no authenticated user and we are not on the login, signup, or root page
-    if (!user.value && !publicLinks.includes(to.path)) {
+    if (!authUser.value && !publicLinks.includes(to.path)) {
         return navigateTo('/auth/login')
+    }
+
+    else if (authUser.value && publicLinks.includes(to.path)) {
+        return navigateTo('/dashboard')
     }
 })
